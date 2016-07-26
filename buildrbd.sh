1d4eeed22cd6e0e0966f14bc3f5eedb207aa8813
Victor Araujo <ve.ar91@gmail.com>
2016-07-26 17:30:28 +0200

    Add instructions for tracing with Ceph and fio rbd engine

--- buildrbd.sh
+++ buildrbd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-cd $HOME/lttng-traces/
+cd ~/lttng-traces/
 rm -r fio
 
-cd $HOME/ceph/build-rbd
+cd ~/ceph/build-rbd
 ../src/stop.sh
 make -j8 || exit
 sudo make install
 ../src/vstart.sh -n -x -d -l --short
 ./bin/rbd -c ./ceph.conf create --size 1024 fio_test --pool rbd
 
-cd $HOME/fio
+cd ~/fio
 LDFLAGS="-L$HOME/ceph/build-rbd/lib -Wl,-rpath,$HOME/ceph/build-rbd/lib" ./configure --extra-cflags="-I/usr/local/include/"
 make
 lttng create fio -o $HOME/lttng-traces/fio
@@ -18,7 +18,7 @@ lttng enable-event -u zipkin:keyval_integer
 lttng enable-event -u zipkin:keyval_string
 lttng start
 
-CEPH_CONF=$HOME/ceph/build-rbd/ceph.conf ./fio $HOME/rbd &
+CEPH_CONF=~/ceph/build-rbd/ceph.conf ./fio ~/rbd &
 FIO_PID=$!
 sleep 3s
 kill $FIO_PID
@@ -26,5 +26,5 @@ kill $FIO_PID
 lttng stop
 lttng destroy
 
-cd $HOME/babeltrace-zipkin/
+cd ~/babeltrace-zipkin/
 python3 babeltrace_zipkin.py $HOME/lttng-traces/fio/ust/uid/1000/64-bit/ -s 127.0.0.1 -p 9410

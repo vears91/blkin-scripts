80470a1f5be6e043954213e33f84a213eba12952
Victor Araujo <ve.ar91@gmail.com>
2016-08-20 12:00:57 +0200

    Include LD_LIBRARY_PATH for running fio

--- buildrbd.sh
+++ buildrbd.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 cd ~/lttng-traces/
-rm -r fio
+rm -rf fio
 
 cd ~/ceph/build-rbd
 ../src/stop.sh
 make -j8 || exit
 sudo make install
-../src/vstart.sh -n -x -d -l --short
+LD_PRELOAD=liblttng-ust-fork.so ../src/vstart.sh -n -x -d -l --short
 ./bin/rbd -c ./ceph.conf create --size 1024 fio_test --pool rbd
 
-cd ~/fio
+if [ -d ~/fio ]
+	then
+	cd ~/fio
+else
+	cd ~
+	git clone https://github.com/axboe/fio && cd ~/fio
+fi
 LDFLAGS="-L$HOME/ceph/build-rbd/lib -Wl,-rpath,$HOME/ceph/build-rbd/lib" ./configure --extra-cflags="-I/usr/local/include/"
 make
 lttng create fio -o $HOME/lttng-traces/fio
@@ -18,7 +24,9 @@ lttng enable-event -u zipkin:keyval_integer
 lttng enable-event -u zipkin:keyval_string
 lttng start
 
-CEPH_CONF=~/ceph/build-rbd/ceph.conf ./fio ~/rbd &
+LD_LIBRARY_PATH=/usr/local/lib
+export LD_LIBRARY_PATH
+CEPH_CONF=~/ceph/build-rbd/ceph.conf ./fio ~/rbd.fio &
 FIO_PID=$!
 sleep 3s
 kill $FIO_PID
